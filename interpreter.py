007b3aff0e08bbe27d3500dc7daea46341692bbf
raymonxst-dot <2xst2raymon.yt@gmail.com>
2024-12-09 11:05:23 +0330

    interp

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,4 @@
-num1,op,num2=input('enter:').split(' ')
+num1,op,num2=input("enter:").split(' ')
 num1=float(num1)
 num2= float(num2)
 #Python are spliting datas between (num1,op,num2) and changing their type to float
