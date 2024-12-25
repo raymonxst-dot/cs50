9786267f092a43ff06b32e4cb5362938232294c9
raymonxst-dot <2xst2raymon.yt@gmail.com>
2024-12-25 19:15:26 +0330

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,7 @@
+user=input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower()
+if user == "forty tow" or user=="42":
+    print("yes")
+elif user == "forty-two":
+    print("yes")
+else:
+    print("no")
