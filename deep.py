eeacf04e8c54cc1bfc84fac569b60487242b2718
raymonxst-dot <2xst2raymon.yt@gmail.com>
2024-12-25 19:18:52 +0330

    deep

--- deep.py
+++ deep.py
@@ -4,4 +4,9 @@ if user == "forty tow" or user=="42":
 elif user == "forty-two":
     print("yes")
 else:
-    print("no")
+    print("no")true_words=["forty-tow","forty tow","42"]
+user=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+if user in true_words :
+    print("Yes")
+else :
+    print("No")
