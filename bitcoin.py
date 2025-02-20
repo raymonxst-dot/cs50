71047a859effa3ec5d36ca851de0d37a066fb7fe
raymonxst-dot <2xst2raymon.yt@gmail.com>
2025-02-20 13:32:30 +0330

    bitcoin

--- bitcoin.py
+++ bitcoin.py
@@ -3,13 +3,13 @@ import sys
 try:
     if len(sys.argv) != 2:
         sys.exit("Missing command-line argument")
-    elif sys.argv[1].isalpha:
+    elif sys.argv[1].isalpha():
         sys.exit("Missing command-line argument")
     else:
         x = requests.get("https://api.coincap.io/v2/assets/bitcoin")
         info=x.json()
         price=info["data"]["priceUsd"]
-        total=float(price)*float(sys.argv[1])
+        total=float(price) * float(sys.argv[1])
         print(f"${total:,.4f}")
 except requests.RequestException:
     pass
